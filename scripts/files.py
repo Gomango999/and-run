11890d9d8898f9ce98537b6c28fbc50579dbd43f
Gomango999 <kv.zhu999@gmail.com>
2020-05-11 14:58:53 +1000

    Documented files.py

--- scripts/files.py
+++ scripts/files.py
@@ -1,39 +1,50 @@
+# Holds scripts pertaining to finding finding cpp and input files.
 import os
 
-# Holds scripts pertaining to finding finding cpp and input files.
-def file_len(fname):
-    with open(fname) as f:
+# Returns the number of lines of a file
+def file_len(filename):
+    with open(filename) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
 
-def file_head(fname, num):
-    with open(fname) as f:
-        return "".join(f.readlines()[0:num])
+# Returns the first n lines of a file.
+def file_head(filename, n):
+    with open(filename) as f:
+        return "".join(f.readlines()[0:n])
 
-# Selects an input file to be used
-# If multiple, selects the first one
+# Automatically selects the file that is most likely the input file
+# An input file is:
+#   - A .txt file ending in 'in.txt',
+#   - A .txt file with input in its name, or
+#   - A .in file
 def get_input_file():
     cwd = os.getcwd()
     input_files = []
     for f in os.listdir(cwd):
-        filename, file_extension = os.path.splitext(f)
+        fileroot, file_extension = os.path.splitext(f)
+        fileroot = fileroot.lower()
+
         # Find default input file
         if ".txt" == file_extension:
-            if "in" == filename.lower()[-2:]:
+            if "in" == fileroot[-2:]:
                 input_files.append(f)
-            elif "input" in filename:
+            elif "input" in fileroot:
                 input_files.append(f)
         elif ".in" == file_extension:
             input_files.append(f)
+
     input_files.sort()
 
     if len(input_files) == 0:
         return None
     return input_files[0]
 
-# Selects a cpp file to be used
-# If multiple, selects the first one
+# Selects the most likely cpp file to be used
+# A cpp file
+#   - ends in .cpp, and
+#   - does not contain test in its name
+# TODO: Make it more likely to match with files that match the folder name
 def get_cpp_file():
     cwd = os.getcwd()
     cpp_files = []
@@ -50,6 +61,7 @@ def get_cpp_file():
         return None
     return cpp_files[0]
 
+# Returns the current basename
 def get_basename():
     cwd = os.getcwd()
     return os.path.basename(os.path.normpath(cwd))
