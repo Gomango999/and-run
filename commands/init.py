9d8aaefe537a56bfdaf027d84a4f37cb1864f986
Gomango999 <kv.zhu999@gmail.com>
2020-10-12 13:52:45 +1100

    Added creation of in.txt and fixed bug

--- commands/init.py
+++ commands/init.py
@@ -19,6 +19,9 @@ def init(type, filename):
     if filename == None:
         filename = get_basename() + ".cpp"
 
+    if filename[-4:] != ".cpp":
+        filename = filename + ".cpp"
+
     if os.path.exists(os.path.join(os.getcwd(), filename)):
         print("File already exists")
         exit(FAILED_CODE)
@@ -28,4 +31,10 @@ def init(type, filename):
         print(f"Successfully created {filename} using the \"{type}\" template")
     else:
         print("Template initialisation failed")
+
+    exit_code = os.system(f"touch in.txt")
+    if (exit_code == SUCCESS_CODE):
+        print(f"Successfully created in.txt")
+    else:
+        print("Input file failed")
     exit(exit_code)
