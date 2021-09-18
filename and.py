49ffe23a2bcdb9ed0b19e953ab8cc1c2450d20f1
Gomango999 <kv.zhu999@gmail.com>
2021-09-18 22:44:25 +1000

    Removed obsolete commands

--- and.py
+++ and.py
@@ -5,10 +5,6 @@ from commands.run import run
 from commands.initletters import initletters
 from commands.init import init
 from commands.edit import edit
-# from commands.done import done
-# from commands.check import check
-
-
 
 @click.group()
 def And():
@@ -18,8 +14,6 @@ And.add_command(run)
 And.add_command(initletters)
 And.add_command(init)
 And.add_command(edit)
-# And.add_command(done)
-# And.add_command(check)
 
 if __name__ == "__main__":
     And()
