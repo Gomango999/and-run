31aadc6627203a07475722b4b861404e8dd854b0
Gomango999 <kv.zhu999@gmail.com>
2022-05-13 15:44:04 +1000

    Fix config code

--- commands/config.py
+++ commands/config.py
@@ -5,11 +5,11 @@ import sys
 sys.path.append('../')
 
 from scripts.files import get_root_dir
-from config.config import config
+from config.config import config as _config # avoid confusion with function name
 
 @click.command()
 def config():
     root = get_root_dir()
     print(root)
-    os.system(f"config['editor_command'] {root}")
-    os.system(f"config['editor_command'] {root}/config/config.py")
+    os.system(f"{_config['editor_command']} {root}")
+    os.system(f"{_config['editor_command']} {root}/config/config.py")
